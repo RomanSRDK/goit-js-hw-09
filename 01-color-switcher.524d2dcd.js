986f28246a9c97966476c588fde010023a42dcb6
RomanSRDK <100283300+RomanSRDK@users.noreply.github.com>
2023-07-11 15:44:57 +0000

    Deploying to gh-pages from @ RomanSRDK/goit-js-hw-09@4f7b9d63d07574ade07c6f4559ddb9e0e2e95c17 🚀

---
+++ 01-color-switcher.524d2dcd.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");e.addEventListener("click",(function(e){e.target.disabled=!0,a.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(a){var n=a.target;e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.524d2dcd.js.map
